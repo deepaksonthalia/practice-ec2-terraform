f66b48122b47af4e3cf084551146e6eba642f308
deepaksonthalia <162591141+deepaksonthalia@users.noreply.github.com>
2025-08-18 11:54:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,7 +23,11 @@ variable "subnet_cidr" {
 variable "instance_type" {
     description = "EC2 instance type"
     type        = string 
-    default     = "t2.micro"  
+    default     = "t2.micro" 
+    validation {
+    condition     = contains(["t2.micro", "t3.micro", "t3.small"], var.instance_type)
+    error_message = "Instance type must be t2.micro, t3.micro, or t3.small."
+  }
 }
 
 #ISO image
