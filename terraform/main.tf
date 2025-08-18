43aebdfec464f11f6718f64e821838585b1c45b3
deepaksonthalia <162591141+deepaksonthalia@users.noreply.github.com>
2025-08-18 15:41:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
-#Configure Terraform Cloud and AWS provider
+# Configure Terraform Cloud and AWS provider
 terraform {
-  #Configure Terraform Cloud at backend
+  # Configure Terraform Cloud at backend
   cloud {
     organization    = "Deep_Space"
     workspaces {
       name          = "practice-ec2-terraform"
     }
   }
-  #Cloud Provider
+  # Cloud Provider
   required_providers {
     aws = {
         source  = "hashicorp/aws"
@@ -16,35 +16,37 @@ terraform {
   }
 }
 
-#Configure AWS provider
+# Configure AWS provider
 provider "aws" {
     region = var.aws_region  
 }
-#Create VPC
+# Create VPC
 resource "aws_vpc" "main" {
     cidr_block              = var.vpc_cidr
     enable_dns_hostnames    = true
     enable_dns_support      = true
+
     tags = {
-        Name                = "main-vpc"
-        Environment         = var.environment 
+        Name        = "main-vpc"
+        Environment = var.environment 
     }   
 }
 
-#Create Public Subnet
+# Create Public Subnet
 resource "aws_subnet" "public" {
     vpc_id                  = aws_vpc.main.id
     cidr_block              = var.subnet_cidr
     #availability_zone       = "$(var.aws_region)a"
     availability_zone       = "${var.aws_region}a"
     map_public_ip_on_launch = true
+
     tags                    = {
         Name                = "public-subnet"
         Environment         = var.environment
     }   
 }
 
-#Create Internet Gateway
+# Create Internet Gateway
 resource "aws_internet_gateway" "main" {
     vpc_id          = aws_vpc.main.id
     tags            = {
@@ -53,7 +55,7 @@ resource "aws_internet_gateway" "main" {
     }  
 }
 
-#Route Table
+# Route Table
 resource "aws_route_table" "public" {
     vpc_id          = aws_vpc.main.id
     route {
@@ -66,13 +68,13 @@ resource "aws_route_table" "public" {
   }
 }
 
-#Associate route table with subnet
+# Associate route table with subnet
 resource "aws_route_table_association" "public" {
     subnet_id       = aws_subnet.public.id
     route_table_id  = aws_route_table.public.id  
 }
 
-#Create security group
+# Create security group
 resource "aws_security_group" "ec2_sg" {
     name            = "ec2-security-group"
     vpc_id          = aws_vpc.main.id
@@ -85,7 +87,7 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks     = ["0.0.0.0/0"]
    }
 
-    #outbound rules
+    # outbound rules
    egress {
     from_port       = 0
     to_port         =  0
@@ -99,7 +101,7 @@ resource "aws_security_group" "ec2_sg" {
    }  
 }
 
-#Create EC2 instance
+# Create EC2 instance
 resource "aws_instance" "web" {
     ami             = var.instance_ami
     instance_type   = var.instance_type
