53932ef540c4cecd6a3241a9d67a7aafb8b1deef
deepaksonthalia <162591141+deepaksonthalia@users.noreply.github.com>
2025-08-19 11:42:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,3 +115,33 @@ resource "aws_instance" "web" {
       Environment   = var.environment
     }  
 }
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "security_group_id" {
+  description = "The ID of the EC2 security group"
+  value       = aws_security_group.ec2_sg.id
+}
+
+output "ec2_instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
